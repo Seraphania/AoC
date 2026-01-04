63f74a09fd346f0539a1b10e7e68c8f7001a2bec
“Seraphania” <strings333@hotmail.com>
2026-01-05 07:15:06 +0800

    .

--- 2025/solutions/day2.cs
+++ 2025/solutions/day2.cs
@@ -1,4 +1,6 @@
-﻿namespace _2025
+﻿using System.Security.Cryptography;
+
+namespace _2025
 {
     [Solution(Day = "2.1", Description = "Gift Shop")]
     class DayxPart1 : ISolution
@@ -45,9 +47,65 @@
     {
         public void Run(List<string> input)
         {
-            // Solution Here
+            Console.WriteLine($"{input.Count} Input Lines Parsed,");
+            string[] ranges = input[0].Split(',');
+            Int64 totalOfInvalidIDs = 0;
 
-            Console.WriteLine($"The Answer is ....");
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                string[] range = ranges[i].Split('-');
+                List<string> ids = GetIDS(range[0], range[1]);
+                Console.WriteLine($"Range: {range[0]} - {range[1]} ");
+                for (int j = 0; j < ids.Count; j++)
+                {
+                    PatternSearch(ids[j]);
+                }
+            }
+            Console.WriteLine($"The Answer is {totalOfInvalidIDs}");
+
+            void PatternSearch(string id)
+            {
+                for (int section = id.Length / 2; section > 0; section--)
+                {
+                    string val = id[0..section];
+                    List<string> substrings = new List<string>();
+                    for (int startIndex = 0; startIndex < id.Length - section+1; startIndex += section)
+                        {
+                            int end = startIndex + section;
+                            substrings.Add(id[startIndex..end]);
+                        }
+                    int check = 0;
+                    foreach (string substring in substrings)
+                    {
+                        if (substring != val)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            check++;
+                            continue;
+                        }
+                    }
+                    if (check == substrings.Count)
+                    {
+                        Console.WriteLine($"\t Invalid ID: {id}");
+                        totalOfInvalidIDs += Int64.Parse(id);
+                        break;
+                    }
+                }
+            }
+
+            List<string> GetIDS(string start, string end)
+            {
+                var ids = new List<string>();
+                for (long i = Int64.Parse(start); i <= Int64.Parse(end); i++)
+                {
+                    string id = i.ToString();
+                    ids.Add(id);
+                }
+                return ids;
+            }
         }
     }
 }
